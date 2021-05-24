8ff95a3a59315e085021bb839228bebcba094ccb
Lev Savolsky <lev.savolskyy@gmail.com>
2021-05-24 23:56:14 +0300

    gg

---
+++ gh-pages.js
@@ -0,0 +1,16 @@
+var ghpages = require('gh-pages');
+
+ghpages.publish(
+    'public', // path to public directory
+    {
+        branch: 'gh-pages',
+        repo: 'https://github.com/ketorg0z/ketorg0z.github.io.git', // Update to point to your repository  
+        user: {
+            name: 'ketorg0z', // update to use your name
+            email: 'lev.savolskyy@gmail.com' // Update to use your email
+        }
+    },
+    () => {
+        console.log('Deploy Complete!')
+    }
+)
